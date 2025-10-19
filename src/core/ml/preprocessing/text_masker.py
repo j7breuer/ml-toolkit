4b0f427b7295c39b36d722a25f91710cc17535d0
J Breuer <jmbreuer@vt.edu>
2025-10-19 01:35:25 -0400

    Adding additional starter scripts

---
+++ src/core/ml/preprocessing/text_masker.py
@@ -0,0 +1,49 @@
+
+import re
+
+class TextMasker:
+    '''
+        desc: A class for masking common patterns in text data
+    '''
+    @staticmethod
+    def mask_email(text: str, replacement_value: str = '<EMAIL>') -> str:
+        '''
+            desc:
+            input:
+            output:
+        '''
+        # Mask email addresses
+        return ''
+
+
+    @staticmethod
+    def mask_phone_number(text: str, replacement_value: str = '<PHONE>') -> str:
+        '''
+            desc:
+            input:
+            output:
+        '''
+        # Mask phone numbers
+        return ''
+    
+
+    @staticmethod
+    def mask_url(text: str, replacement_value: str = '<URL>') -> str:
+        '''
+            desc:
+            input:
+            output:
+        '''
+        # Mask URLs
+        return ''
+    
+
+    @staticmethod
+    def mask_custom_value(text: str, pattern: str, replacement_mask: str) -> str:
+        '''
+            desc:
+            input:
+            output:
+        '''
+        # Mask custom patterns
+        return ''
